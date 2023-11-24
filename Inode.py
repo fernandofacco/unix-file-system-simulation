48d1d34bae951a23aca921cbcce0e6fa16f87be3
Fernando Facco Rodrigues <fernandofaccor@gmail.com>
2023-11-23 21:32:50 -0300

    Changes into inodeInfo function

--- Inode.py
+++ Inode.py
@@ -7,17 +7,25 @@ class Inode:
         self.lastAccessedDate = self.createdDate
         self.lastModifiedDate = self.createdDate
         self.permissions = {
-            "user": {"read": True, "write": True, "execute": True},
-            "group": {"read": True, "write": True, "execute": True},
+            "user": {"read": True, "write": True, "execute": False},
+            "group": {"read": True, "write": False, "execute": False},
             "public": {"read": True, "write": False, "execute": False},
         }   
         self.isDirectory = isDirectory
         self.size = 0
-        self.blocks = [None] * 10
+        self.blocks = []
         self.indirect_block = None
 
     def inodeInfo(self):
         if (self.isDirectory):
-            return f"  Size: {self.size} Blocks: {self.blocks}        directory\n   Uid: {self.ownerId}\nAccess: {self.lastAccessedDate} \nModify: {self.lastModifiedDate} \n Birth: {self.createdDate}"
+            return f"  Size: {self.size}               Blocks: {len(self.blocks)}               directory\nAccess: ({self.buildPermissionStringFormat()})    Uid: {self.ownerId}\nAccess: {self.lastAccessedDate} \nModify: {self.lastModifiedDate} \n Birth: {self.createdDate}"
         else:
-            return f"  Size: {self.size} Blocks: {self.blocks}        file\n   Uid: {self.ownerId}\nAccess: {self.lastAccessedDate} \nModify: {self.lastModifiedDate} \n Birth: {self.createdDate}"
+            return f"  Size: {self.size}               Blocks: {len(self.blocks)}               file\nAccess: ({self.buildPermissionStringFormat()})    Uid: {self.ownerId}\nAccess: {self.lastAccessedDate} \nModify: {self.lastModifiedDate} \n Birth: {self.createdDate}"
+        
+    def buildPermissionStringFormat(self):
+        permissionString = "-"
+        for scope, permission in self.permissions.items():
+            permissionString += "r" if permission["read"] else "-"
+            permissionString += "w" if permission["write"] else "-"
+            permissionString += "x" if permission["execute"] else "-"
+        return permissionString
