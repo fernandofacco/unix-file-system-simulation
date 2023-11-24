9d8917d1b41839a653721b773c2874bb13acdc43
Fernando Facco Rodrigues <fernandofaccor@gmail.com>
2023-11-23 21:31:46 -0300

    Added isAdmin attribute in User.py

--- User.py
+++ User.py
@@ -1,6 +1,7 @@
 class User:
-    def __init__(self, username, userId, password):
+    def __init__(self, username, userId, password, isAdmin):
         self.username = username
         self.password = password
         self.userId = userId
+        self.isAdmin = isAdmin
 
